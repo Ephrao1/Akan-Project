18c4cc15b5154cdd77eed6a63c53ccca3181befa
Ephraim Bundi <ebundi04@gmail.com>
2021-07-18 11:58:06 +0300

    Add an Array

---
+++ index.js
@@ -0,0 +1,19 @@
+let dayNames = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"]
+let maleNames = ["Kwasi","Kwadwo","Kwabena","Kwaku","Yaw","Kofi","Kwame"]
+let femaleNames = ["Akosua","Adwoa","Abenaa","Akua","Yaa","Afua","Ama"]
+
+function validateForm() {
+    var year = document.getElementById("year").value;
+    var month = document.getElementById("month").value;
+    var day = document.getElementById("day").value;
+
+    if (month < 1 || month > 12) {
+        alert("Insert correct month betwen 1 and 12"); return false;
+    }
+    if (day < 1 || day > 31) {
+        alert("Provide a valid year of birth e.g 2021"); return false;
+    }
+    if (month < 1 || day > 12) {
+        alert("Insert correct month betwen 1 and 12"); return false;
+    }
+}
